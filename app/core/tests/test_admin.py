d2db2698ea4e27e023f9fd3011f7d8b04c5c0c84
satyajeet_jha <mac@Macs-MacBook-Air.local>
2020-01-20 00:08:25 +0530

    wait for db and postgres added

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -19,15 +19,22 @@ class AdminSiteTests(TestCase):
         )
     
         
-     def test_users_listed(self):
-         """Test that users are listed on the user page"""
+    def test_users_listed(self):
+        """Test that users are listed on the user page"""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
         
-     def test_user_page_change(self):
+    def test_user_page_change(self):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
-        self.assertEqual(res.status_code, 200)     
+        self.assertEqual(res.status_code, 200)    
+        
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)    
